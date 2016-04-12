b72c9f61fc7418cd772f65b4597bd8675891bc59
Brian Wolk <Brian@Aftermac.local>
2016-04-11 22:15:04 -0400

    initial commit

---
+++ windowcom.js
@@ -0,0 +1,55 @@
+function log(msg){
+	$("#log").append(msg + "<br/>");
+}
+
+function openWindow(url){
+	window.open(url);
+}
+
+function signIn(){
+	sessionStorage.setItem("signInTime", moment().valueOf());
+
+	function timeToCompareTo() {
+		return (sessionStorage.getItem("lastAlertTime") || sessionStorage.getItem("signInTime"));
+	}
+
+	function si() {
+		console.log("si!");
+		var diffInSec = moment().diff(moment(Number(timeToCompareTo())), "seconds");
+		console.log("diffInSec is ", diffInSec);
+		if ( diffInSec >= 10) {
+			alert("10 or more seconds since sign in");
+			sessionStorage.setItem("lastAlertTime", moment().valueOf());
+		}
+	}
+
+	window.si = setInterval(si, 500);
+
+	$("#signIn").attr("disabled", "disabled");
+	$("#signOut").removeAttr("disabled");
+}
+
+function signOut(){
+	clearInterval(window.si);
+
+	$("#signIn").removeAttr("disabled");
+	$("#signOut").attr("disabled", "disabled");
+}
+
+$(document).ready(function(){
+
+	$(document).on("visibilitychange", function(){
+		if (!document.hidden) {
+			log("document is visible!");
+		}
+	});
+
+	$("#newWindow").on("click", function(){
+		openWindow(window.location.href);
+	});
+
+	$("#signIn").on("click", signIn);
+
+	$("#signOut").on("click", signOut);
+
+});
